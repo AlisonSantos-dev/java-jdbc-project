c4b4d0fbf882a72eed401351f0da6f87af7d194d
Alison Santos <alissonsouza922@gmail.com>
2026-06-02 00:12:44 -0300

    SellerDaoJDBC and DaoFactory - Implementação Classe SellerDaoJDBC e DaoFactory

--- src/test/java/application/Program.java
+++ src/test/java/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -17,6 +19,8 @@ public class Program {
                 new Date(),
                 4000.0,
                 obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
