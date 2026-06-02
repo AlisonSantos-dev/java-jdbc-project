6df849ed9e20ca66419ef6d5a3391d1a63f0e832
Alison Santos <alissonsouza922@gmail.com>
2026-06-01 23:45:20 -0300

    Seller entity class - Implementação Classe Vendedor

--- src/test/java/application/Program.java
+++ src/test/java/application/Program.java
@@ -1,13 +1,24 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21,
+                "Alison Santos",
+                "alissonsouza922@gmail.com",
+                new Date(),
+                4000.0,
+                obj);
+
+        System.out.println(seller);
 
 
 
